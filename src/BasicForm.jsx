0bbf2c39dd5c74517586bdb11170c1ffd0f18330
nymch <ko@i-19.ch>
2019-04-25 12:08:09 +0900

    add rough email validation style fix

--- src/BasicForm.jsx
+++ src/BasicForm.jsx
@@ -25,12 +25,13 @@ const validate = (values) => {
 const regex = {
   text: (length) => new RegExp(`^[a-zA-Z0-9]${length ? '{1,' + length + '}' : '+'}$`),
   number: (length) => new RegExp(`^[0-9]${length ? '{1,' + length + '}' : '+'}$`),
+  email: () => new RegExp(`^[a-zA-Z0-9\\.\\-_]+@([a-zA-Z0-9\\-]+\\.)+[a-z]+$`),
   date: (separator) => new RegExp(`(19|20)[0-9]{2}\\${separator ? separator : '/'}(0[1-9]|1[0-2])\\${separator ? separator : '/'}(0[1-9]|[1-2][0-9]|3[0-1])`),
 }
 
 const schema = {
   handleName: regex.text(20),
-  email: regex.text(),
+  email: regex.email(),
   birthDate: regex.date('/'),
   hobby: regex.text(100),
 }
@@ -58,32 +59,38 @@ const BasicForm = () => {
         actions.setFieldValue(formInitialValues)
       }}
       render={({ errors, status, touched, isSubmitting, isValid }) => (
-        <Form autocomplete="off">
+        <Form autocomplete="off" className="form">
           <div>
+            <label for="handleName">Handle name: </label>
             <Field type="text" name="handleName" className="form-input" placeholder="JohnDoe" />
-            {errors.handleName && touched.handleName && <div className="form-error">{errors.handleName}</div>}
+            {errors.handleName && touched.handleName && <span className="form-error">{errors.handleName}</span>}
           </div>
           <div>
+            <label for="email">Email: </label>
             <Field type="email" name="email" className="form-input" placeholder="john@doe.gg"/>
-            {errors.email && touched.email && <div className="form-error">{errors.email}</div>}
+            {errors.email && touched.email && <span className="form-error">{errors.email}</span>}
           </div>
           <div>
+            <label for="handleName">Birth date: </label>
             <Field type="text" name="birthDate" className="form-input" placeholder="1970/01/01" />
-            {errors.birthDate && touched.birthDate && <div className="form-error">{errors.birthDate}</div>}
+            {errors.birthDate && touched.birthDate && <span className="form-error">{errors.birthDate}</span>}
           </div>
           <div>
+            <label for="handleName">Hobby: </label>
             <Field type="text" name="hobby" className="form-input" placeholder="Sleeping" />
-            {errors.hobby && touched.hobby && <div className="form-error">{errors.hobby}</div>}
+            {errors.hobby && touched.hobby && <span className="form-error">{errors.hobby}</span>}
           </div>
-          {isSubmitting && <div>submitting.....</div>}
-          {status && status.message && <div className="form-error">{status.message}</div>}
-          {isValid && <div style={{color:"green",fontSize:16,fontWeight:"bolder"}}>Valid input!</div>}
-          <button type="submit" disabled={isSubmitting || !isValid}>
-            Submit
-          </button>
-          <button type="reset">
-            Reset
-          </button>
+          <div>
+            {isSubmitting && <div>submitting.....</div>}
+            {status && status.message && <span className="form-error">{status.message}</span>}
+            {isValid && <span className="form-valid">Valid input!</span>}
+            <button type="submit" disabled={isSubmitting || !isValid}>
+              Submit
+            </button>
+            <button type="reset">
+              Reset
+            </button>
+          </div>
         </Form>
       )}/>
   )
